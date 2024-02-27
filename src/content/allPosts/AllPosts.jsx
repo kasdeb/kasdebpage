a613332aa039c456445be38304be8d84a85836a3
Kasper Debicki <kasper.debicki92@gmail.com>
2024-02-27 13:11:51 +0100

    added post5

--- src/content/allPosts/AllPosts.jsx
+++ src/content/allPosts/AllPosts.jsx
@@ -1,6 +1,13 @@
+import post5 from "../posts/post_5/post_5";
 import post4 from "../posts/post_4/post_4";
 import post3 from "../posts/post_3/post_3";
 import post2 from "../posts/post_2/post_2";
 import post1 from "../posts/post_1/post_1";
 
-export const allPosts = [post4, post3, post2, post1];
+export const allPosts = [
+  post5,
+  post4,
+  post3,
+  post2,
+  post1
+];
---
+++ src/content/posts/post_5/post_5.jsx
@@ -0,0 +1,42 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialOceanic } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import bgc from "../post_5/bgc2.jpg";
+
+let customObjectData_cList = "";
+
+let post_5 = {
+  id: 2,
+  new: false,
+  postedDate: "02-27-2024",
+  updatedDate: "02-27-2024",
+  postedBy: "Kasper Debicki",
+  title: "Marketo Custom Object + Velocity", // title with spaces
+  url: "marketo-custom-object-+-velocity", // should be the same as title but instead of spaces use dashes
+  tags: ["#velocity", "#marketo"],
+  backgroundImage: bgc, // tile image
+  post: (
+    <>
+      <div className="post-text">
+        <b>Marketo Custom Object + Velocity</b>
+        <br />
+        At times, you may want to send emails even
+        to those who don't have a custom object on
+        their record. This snippet allows you to
+        apply logic to anyone, whether they have
+        or haven't a custom object available.
+        <SyntaxHighlighter
+          wrapLines={true}
+          language="velocity"
+          style={materialOceanic}
+        >
+          {`#if (\${customObjectData_cList.get(0).name} && !\${customObjectData_cList.get(0).name.isEmpty()})
+##custom object available
+#else
+##custom object NOT available
+#end`}
+        </SyntaxHighlighter>
+      </div>
+    </>
+  )
+};
+export default post_5;
